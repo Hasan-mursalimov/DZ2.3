9eba2bf76e570259b26eaa6d48564e54a198aae9
hasan <hasan1993mur@gmail.com>
2021-03-14 14:37:28 +0500

    дальше не знаю что делать

--- Person/src/Main.java
+++ Person/src/Main.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
+
 
         }
     }
--- Person/src/SortPerson.java
+++ Person/src/SortPerson.java
@@ -5,14 +5,18 @@ public class SortPerson {
         for (int j = 0; j < personList.size(); j++) {
             Person min = personList.get(j);
             int min_j = j;
-            for (int i = 0; i < personList.size(); i++) {
-                if (personList.get(j - 1).compareTo(personList.get(j)) > 0) {
-                    if (personList.get(j).compareTo(min) < 0) {
-                        min = personList.get(j);
-                        min_j = i;
-                    }
-                }
+            if (personList.get(j).compareTo(min) < 0) ;
+            {
+                min = personList.get(j);
+                min_j = j;
+            }
+            if (j != min_j) {
+                Person prs = personList.get(j);
+                personList.set(j, personList.get(min_j));
+                personList.set(min_j, prs);
             }
         }
     }
 }
+
+
