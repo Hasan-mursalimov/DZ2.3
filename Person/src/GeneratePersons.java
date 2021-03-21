ba40015c069b713ffa09a71b8ba81ea802f3aa6a
hasan <hasan1993mur@gmail.com>
2021-03-21 15:07:51 +0500

    изменил метод generateAge, для рандомного значения age

--- Person/src/GeneratePersons.java
+++ Person/src/GeneratePersons.java
@@ -1,34 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
-public class GeneratePersons  {
 
-    private static Random random=new Random();
+public class GeneratePersons {
 
-    public List<Person> generatePersons(){
-        List<Person>personList=new ArrayList<>();
+    private static Random random = new Random();
+
+    public List<Person> generatePersons() {
+        List<Person> personList = new ArrayList<>();
         //цикл для создания 10000 персон
-        for (int i=0;i<10001;i++){
-            personList.add(new Person( generateName(),generateAge(),generateSex()));
+        for (int i = 0; i < 10001; i++) {
+            personList.add(new Person(generateName(), generateAge(), generateSex()));
         }
         return personList;
     }
 
 
-    public String generateName(){
+    public String generateName() {
         //рандомное значение из массива
-        String [] name=new String[]{"fff","sss","sass","sdasd","sdasd"};
-        String randomName=name[random.nextInt(name.length)];
+        String[] name = new String[]{"fff", "sss", "sass", "sdasd", "sdasd"};
+        String randomName = name[random.nextInt(name.length)];
         return randomName;
     }
-    public int generateAge(){
+
+    public int generateAge() {
+        int ageArr[] = new int[100];
+        for (int i = 0; i < ageArr.length; i++) {
+            ageArr[i] = i;
+        }
         //при попытке вычесления рандомного значения выводит ошибка IllegalArgumentException, пока решить не смог
-        int  age=100;
+        int age = ageArr[random.nextInt(ageArr.length)];
         return age;
     }
-    public static Sex generateSex(){
+
+    public static Sex generateSex() {
         return Sex.values()[random.nextInt(Sex.values().length)];
     }
 }
